a411d16375c9b91a02549035cf42d1108b898a9f
Ege Gürsel <gurege.24@robcol.k12.tr>
2021-12-30 11:32:51 +0300

    fixed music commands

--- cogs/music.py
+++ cogs/music.py
@@ -6,59 +6,94 @@ from discord.ext import commands
 from urllib.request import Request, urlopen
 from __main__ import env
 
+FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+                  "options": "-vn"}
+YDL_OPTIONS = {"format": "bestaudio"}
 queue_list = []
 sıra_boş_embed = discord.Embed(description="**▶️  Şarkı sırası boş.**", color=discord.Color.red())
+novoice_embed = discord.Embed(
+    description="**❌  Bu komutu kullanmak için bir ses kanalına bağlanın.**",
+    color=discord.Color.red())
+samechan_embed = discord.Embed(
+    description="**❌  Bu komutu kullanmak için botun olduğu ses kanalına bağlanın.**",
+    color=discord.Color.red())
+dc_embed = discord.Embed(
+    description="**▶️  Ses kanalından çıkıldı.**",
+    color=discord.Color.red())
+
 
 class MusicCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        @bot.command()
-        async def p(ctx, *args):
-            global queue_list, vc, video_ids, FFMPEG_OPTIONS, YDL_OPTIONS
-            search_term = ('{}'.format("+".join(args)))
+        async def yt_search():
+            global currentvidlink
             request = Request("https://www.youtube.com/results?search_query=" + search_term)
             request.add_header(env["BOT_TOKEN"], "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ("
                                                  "KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
             response = urlopen(request)
             video_ids = re.findall(r"watch\?v=(\S{11})", response.read().decode())
-            queue_list.append("https://www.youtube.com/watch?v=" + video_ids[0])
-            if len(queue_list) > 1:
-                queue_embed = discord.Embed(
-                    description="**✅  Şarkı sıraya eklendi, şu an çalan şarkıyı geçmek için !skip yazın.**",
-                    color=discord.Color.red())
-                await ctx.send(embed=queue_embed)
-            voiceclient = ctx.guild.voice_client
-            if not voiceclient:
-                try:
-                    await ctx.author.voice.channel.connect()
-                except:
-                    novoice_embed = discord.Embed(
-                        description="**❌  Bu komutu kullanmak için bir ses kanalına bağlanın.**",
-                        color=discord.Color.red())
-                    await ctx.send(embed=novoice_embed)
-            vc = ctx.voice_client
-            FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
-                              "options": "-vn"}
-            YDL_OPTIONS = {"format": "bestaudio"}
+            currentvidlink = "https://www.youtube.com/watch?v=" + video_ids[0]
+            queue_list.append(currentvidlink)
+
+        async def play():
+            global audio_source
             with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
                 info = ydl.extract_info(url=queue_list[0], download=False)
                 url_2 = info["formats"][0]["url"]
                 audio_source = await discord.FFmpegOpusAudio.from_probe(url_2, **FFMPEG_OPTIONS)
                 if len(queue_list) == 1:
-                    vc.play(source=audio_source, after=lambda e: asyncio.run(skip(ctx)))
-                    await ctx.send("**Çalınan Parça: " + queue_list[0])
+                    vc.play(source=audio_source)
+                elif len(queue_list) > 1:
+                    queue_list.append(audio_source)
+
+        @bot.command()
+        async def self_check(ctx):
+            while vc.is_playing():
+                await asyncio.sleep(3)
+            else:
+                if len(queue_list) != 0:
+                    await ctx.invoke(bot.get_command("skip"))
+                else:
+                    await asyncio.sleep(20)
+                    await ctx.invoke(bot.get_command("dc"))
+
+        @bot.command()
+        async def p(ctx, *args):
+            global queue_list, vc, search_term
+            search_term = ('{}'.format("+".join(args)))
+            if not ctx.guild.voice_client:
+                try:
+                    await ctx.author.voice.channel.connect()
+                    vc = ctx.voice_client
+                except:
+                    await ctx.send(embed=novoice_embed)
+                    return
+            await yt_search()
+            await play()
+            if queue_list[0] == currentvidlink:
+                await ctx.send("**Çalınan Parça:** " + str(queue_list[0]))
+            else:
+                queue_embed = discord.Embed(
+                    description="**✅  Şarkı sıraya eklendi, şu an çalan şarkıyı geçmek için !skip yazın.**",
+                    color=discord.Color.red())
+                await ctx.send(embed=queue_embed)
+            await ctx.invoke(bot.get_command("self_check"))
 
         @bot.command()
         async def dc(ctx):
             try:
                 queue_list.clear()
                 await ctx.voice_client.disconnect()
+                await ctx.send(embed=dc_embed)
             except:
                 return
 
         @bot.command()
         async def skip(ctx):
+            if not ctx.author.voice.channel == ctx.guild.voice_client.channel:
+                await ctx.send(embed=samechan_embed)
+                return
             skip_embed = discord.Embed(description="**✅  Şarkı geçildi.**", color=discord.Color.red())
             try:
                 del queue_list[0]
@@ -68,25 +103,20 @@ class MusicCog(commands.Cog):
                 vc.stop()
                 connected = False
                 while not connected:
-                    try:
-                        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-                            info = ydl.extract_info(url=queue_list[0], download=False)
-                            url_2 = info["formats"][0]["url"]
-                            audio_source = await discord.FFmpegOpusAudio.from_probe(url_2, **FFMPEG_OPTIONS)
-                            connected = True
-                            await asyncio.sleep(1)
-                    except:
-                        pass
-                vc.play(source=audio_source, after=lambda e: asyncio.run(skip(ctx)))
+                    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+                        info = ydl.extract_info(url=queue_list[0], download=False)
+                        url_2 = info["formats"][0]["url"]
+                        audio_source = await discord.FFmpegOpusAudio.from_probe(url_2, **FFMPEG_OPTIONS)
+                        connected = True
+                vc.play(source=audio_source)
                 await ctx.send(embed=skip_embed)
-                await ctx.send("**Çalınan Parça: "+queue_list[0])
+                await ctx.send("**Çalınan Parça:** " + str(queue_list[0]))
             elif len(queue_list) == 0:
                 vc.stop()
                 await ctx.send(embed=skip_embed)
                 await ctx.send(embed=sıra_boş_embed)
-                await asyncio.sleep(30)
-                await vc.disconnect()
                 return
+            await ctx.invoke(bot.get_command("self_check"))
 
         @bot.command()
         async def queue(ctx):
@@ -95,7 +125,7 @@ class MusicCog(commands.Cog):
                     sıra_embed = discord.Embed(title="**Şarkı Sırası:**", color=discord.Color.red())
                     await ctx.send(embed=sıra_embed)
                     for i in range(len(queue_list)):
-                        await ctx.send("**" + str(i+1) + ")** " + queue_list[i])
+                        await ctx.send("**" + str(i + 1) + ")** " + queue_list[i])
                 else:
                     await ctx.send(embed=sıra_boş_embed)
             except:
